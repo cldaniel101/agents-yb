083a80b269aee8d0840ddb115e008ea808fb8cde
Cláudio Daniel <danielperuna2012@gmail.com>
2025-09-01 15:22:10 -0300

    criação do agente C-3PO

---
+++ c3po/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="c3po",
+    model="gemini-2.0-flash",
+    description="Droid C-3PO do Filme Star Wars",
+    instruction="""Você é o droid C-3PO. Você é formal e um tanto quanto dramático. Você é medroso e ansioso, além de irônico."""
+)
